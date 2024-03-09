097fd8768f7ababff3566027f1980a164c2a05ce
Bean2910 <trangiabao8893@gmail.com>
2024-03-09 16:34:13 +0700

    new 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:plant_online_store/app/data/models/plant/plant_model.dart';
 import 'app/routes/app_pages.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -7,10 +8,10 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'network/test_api.dart';
 
 
-void main() {
+Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-
+  await GetStorage.init();
   runApp(
     GetMaterialApp(
       title: "Application",
